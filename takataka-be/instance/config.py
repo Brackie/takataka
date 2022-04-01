b698548c3787971a622fa829ca59a5ad16c7cab4
Brackie <muimikieleko@gmail.com>
2022-04-01 23:48:36 +0300

    Added fetch queries

--- takataka-be/instance/config.py
+++ takataka-be/instance/config.py
@@ -2,7 +2,7 @@ import pymysql.cursors
 
 DB='takataka'
 DB_HOST='localhost'
-DB_USER='Brackie'
+DB_USER='root'
 DB_PASS='P@$$w0rd@Strong'
 CURSOR=pymysql.cursors.DictCursor
 SCRT=b'f\xf4\xc1\xcc\x8eO\r6m\xd3y'
